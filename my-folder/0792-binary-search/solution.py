c1d443ade7710a258bc55a6eb0258317c64f5715
Gouri Rajesh <64693139+GouriRajesh@users.noreply.github.com>
2025-08-13 18:03:13 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.7 MB (73.90%)

---
+++ my-folder/0792-binary-search/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        def binary_search(nums, low, high, target):
+            if low > high:
+                return -1
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                return binary_search(nums, mid + 1, high, target)
+            else:
+                return binary_search(nums, low, mid - 1, target)
+
+        low = 0
+        high = len(nums) - 1
+        res = binary_search(nums, low, high, target)
+
+        return res
+
