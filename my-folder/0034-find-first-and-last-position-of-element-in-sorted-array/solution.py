c4f736b6d24b99e4afad3f1508e59c874efe5359
Gouri Rajesh <64693139+GouriRajesh@users.noreply.github.com>
2025-08-15 18:37:01 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (93.55%)

--- my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
+++ my-folder/0034-find-first-and-last-position-of-element-in-sorted-array/solution.py
@@ -1,14 +1,43 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        if nums == []:
-            return [-1,-1]
-        elif len(nums) == 1 and nums[0] == target:
-            return [0,0]
-        else:
-            first = nums.index(target) if target in nums else -1
+        n = len(nums)
+
+        def first_pos(nums, n, target):
+            low = 0
+            high = n - 1
+            first = -1
+
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] == target:
+                    first = mid
+                    high = mid - 1
+                elif nums[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return first
+
+        def last_pos(nums, n, target):
+            low = 0
+            high = n - 1
             last = -1
-            if(first != -1):
-                nums.sort(reverse = True)
-                last = len(nums) - 1 - nums.index(target)
+
+            while low <= high:
+                mid = (low + high) // 2
+                if nums[mid] == target:
+                    last = mid
+                    low = mid + 1
+                elif nums[mid] < target:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+            return last
+
+        first = first_pos(nums, n, target)
+        if first == -1:
+            return [-1, -1]
+        else:
+            last = last_pos(nums, n, target)
             return [first, last]
 
