538231aceeb2ff37b34ed05c99d89ed86444df35
Gouri Rajesh <64693139+GouriRajesh@users.noreply.github.com>
2025-11-05 23:52:34 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 18.8 MB (51.68%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,13 +1,20 @@
 class Solution:
-    def twoSum(self, nums, target):
-        li=[]
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j]==target:
-                    li.append(i)
-                    li.append(j)
-                    break
-        return li
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # Brute-Force Solution
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
 
-                    
-                
+        # Hap-Map Solution
+        nums_map = {}
+        # Iterate over all numbers in the list
+        for i in range(len(nums)):
+            # Find the complement of current number
+            complement = target - nums[i]
+            # If index of complement is already known through hash-map -> return it
+            if complement in nums_map:
+                return [i, nums_map[complement]]
+            # Else add current element to the hah-map with index
+            nums_map[nums[i]] = i
+
