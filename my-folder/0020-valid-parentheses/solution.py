f161cfae547b00d5e964fdd63f5ab704fb025e59
Gouri Rajesh <64693139+GouriRajesh@users.noreply.github.com>
2025-11-06 04:34:29 +0000

    [LeetCode Sync] Runtime - 2 ms (41.52%), Memory - 17.9 MB (36.65%)

--- my-folder/0020-valid-parentheses/solution.py
+++ my-folder/0020-valid-parentheses/solution.py
@@ -1,30 +1,28 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        # Initialize stack
         stack = []
-        for ch in s:
-            if(ch=="(" or ch=="[" or ch=="{"):
-                stack.append(ch)
-            elif len(stack)==0:
-                return False
-            elif(ch==")"):
-                if stack[len(stack)-1]=="(":
-                    stack.pop()
-                else:
+        # Traverse through entire string
+        for char in s:
+            # If opening bracket -> push to stack
+            if char in ["(", "[", "{"]:
+                stack.append(char)
+            # If closing bracket -> pop from stack and compare
+            if char in [")", "]", "}"]:
+                # If stack is empty -> No matching opening bracket -> return False
+                if len(stack) == 0:
                     return False
-            elif(ch=="]"):
-                if stack[len(stack)-1]=="[":
-                    stack.pop()
-                else:
+                # Pop element from stack and compare
+                stack_element = stack.pop()
+                if (
+                    (char == ")" and stack_element != "(")
+                    or (char == "]" and stack_element != "[")
+                    or (char == "}" and stack_element != "{")
+                ):
                     return False
-            elif(ch=="}"):
-                if stack[len(stack)-1]=="{":
-                    stack.pop()
-                else:
-                    return False
-        
-        if len(stack) ==0:
-            return True
-        else:
-            return False
 
-        
+        # Finally if stack is empty -> return True
+        if len(stack) == 0:
+            return True
+        return False
+
