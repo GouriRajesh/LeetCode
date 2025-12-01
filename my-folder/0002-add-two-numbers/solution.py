cbe89ff9e49dfadd1ec15bfd528520e9a6db9b08
Gouri Rajesh <64693139+GouriRajesh@users.noreply.github.com>
2025-12-01 05:27:01 +0000

    [LeetCode Sync] Runtime - 7 ms (32.87%), Memory - 18 MB (35.40%)

--- my-folder/0002-add-two-numbers/solution.py
+++ my-folder/0002-add-two-numbers/solution.py
@@ -1,64 +1,35 @@
 # Definition for singly-linked list.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
     def addTwoNumbers(
         self, l1: Optional[ListNode], l2: Optional[ListNode]
     ) -> Optional[ListNode]:
-        # Optional Parameters means that the parameters can hold value or can be None
 
-        temp1 = l1
-        temp2 = l2
-        nextvalue = 0
+        # Keep a dummy node and then skip it in the end
+        dummy = ListNode(0)
+        curr = dummy
+        carry = 0
 
-        head = None
-        temp = head
-
-        # Check if both lists contain value
-        while temp1 is not None and temp2 is not None:
-            sum = temp1.val + temp2.val + nextvalue
-            nextvalue = sum // 10
-            currentvalue = sum % 10
-
-            if temp is None:
-                new_node = ListNode(currentvalue)
-                head = new_node
-                temp = new_node
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
+            sum = val1 + val2 + carry
+            if sum >= 10:
+                carry = sum // 10
+                sum = sum % 10
             else:
-                new_node = ListNode(currentvalue)
-                temp.next = new_node
-                temp = new_node
+                carry = 0
+            new_node = ListNode(val=sum)
+            curr.next = new_node
+            curr = new_node
 
-            temp1 = temp1.next
-            temp2 = temp2.next
+            if l1:
+                l1 = l1.next
+            if l2:
+                l2 = l2.next
 
-        while temp1 is not None:
-            sum = temp1.val + nextvalue
-            nextvalue = sum // 10
-            currentvalue = sum % 10
-
-            new_node = ListNode(currentvalue)
-            temp.next = new_node
-            temp = new_node
-            temp1 = temp1.next
-
-        while temp2 is not None:
-            sum = temp2.val + nextvalue
-            nextvalue = sum // 10
-            currentvalue = sum % 10
-
-            new_node = ListNode(currentvalue)
-            temp.next = new_node
-            temp = new_node
-            temp2 = temp2.next
-
-        if nextvalue != 0:
-            new_node = ListNode(nextvalue)
-            temp.next = new_node
-            temp = new_node
-
-        return head
+        return dummy.next
 
